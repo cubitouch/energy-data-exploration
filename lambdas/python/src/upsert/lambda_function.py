af648f0898db21edbbd944a22d0a8195078ed9a3
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-01-05 02:29:33 +0100

    feat: add upsert lambda

---
+++ lambdas/python/src/upsert/lambda_function.py
@@ -0,0 +1,92 @@
+import boto3
+import pandas as pd
+import urllib.parse
+import zipfile
+import dlt
+import os
+import csv
+
+# Initialize S3 and SSM clients
+s3 = boto3.client("s3")
+ssm = boto3.client("ssm")
+
+def lambda_handler(event, context):
+    if event is not None:
+        # Get the bucket name and object key from the event
+        bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+        object_key = event["Records"][0]["s3"]["object"]["key"]
+
+        # Decode the object key
+        decoded_object_key = urllib.parse.unquote(object_key)
+
+        print(f"Processing file: s3://{bucket_name}/{decoded_object_key}")
+
+        # Download the ZIP file from S3
+        zip_file_path = f"/tmp/{decoded_object_key.split('/')[-1]}"
+        s3.download_file(bucket_name, decoded_object_key, zip_file_path)
+    else:
+        # local testing
+        zip_file_path = "/tmp/2025-01-04T16_41_46.338Z.zip"
+
+    # Extract the .xls file from the ZIP
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        # List files in the ZIP archive
+        zip_contents = zip_ref.namelist()
+        print(f"ZIP contents: {zip_contents}")
+
+        # Look for .xls files
+        xls_files = [file for file in zip_contents if file.endswith('.xls')]
+        if not xls_files:
+            raise FileNotFoundError("No .xls file found in the ZIP archive.")
+
+        # Extract the first .xls file
+        xls_file_name = xls_files[0]
+        extracted_xls_path = f"/tmp/{xls_file_name}"
+        zip_ref.extract(xls_file_name, "/tmp")
+
+    print(f"Extracted XLS file: {extracted_xls_path}")
+
+    # Load the file into a Pandas DataFrame
+    # here we enforce 40 columns because the file might be malformed which shifts the cells by 1
+    data = pd.read_csv(extracted_xls_path, sep="\t", encoding="ISO-8859-1", usecols=range(40))
+    print(data.head())  # Print the first few rows for debugging
+    
+    # Use DLT to ingest the data into the database
+    ingest_to_database(data)
+
+    return {"statusCode": 200, "body": f"Processed {object_key}"}
+
+def ingest_to_database(data):
+    # Fetch the DSN from SSM Parameter Store
+    response = ssm.get_parameter(Name="/energy-market-france/db-dsn", WithDecryption=True)
+    database_dsn = response["Parameter"]["Value"]
+
+    # Save the DataFrame to a CSV file in /tmp
+    csv_file_path = "/tmp/energy_market_france.csv"
+    data.to_csv(csv_file_path, index=False)
+    print(f"Data saved to CSV at {csv_file_path}")
+
+    # Define a function to yield rows from the CSV
+    def csv_generator(file_path):
+        with open(file_path, mode="r", encoding="ISO-8859-1") as csv_file:
+            reader = csv.DictReader(csv_file)
+            rows = list(reader)
+            for row in rows[:-3]:  # Skip the last row
+                yield row
+
+    # Initialize DLT pipeline
+    dlt_pipeline = dlt.pipeline(
+        destination=dlt.destinations.postgres(database_dsn),
+        dataset_name="energy_market_france"
+    )
+
+    # Use the generator as a resource
+    resource = dlt.resource(csv_generator(csv_file_path), name="raw_energy_market_france")
+
+    # Run the pipeline with the resource
+    dlt_pipeline.run(resource, write_disposition="replace")
+
+    print("Pipeline run successfully.")
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-north-1" # Replace with your desired default region
+}
+
 provider "aws" {
   # https://aws.amazon.com/fr/blogs/architecture/how-to-select-a-region-for-your-workload-based-on-sustainability-goals/
-  region = "eu-north-1"
+  region = var.aws_region
 }
 
 terraform {
@@ -11,6 +17,10 @@ terraform {
     encrypt        = true
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+# ingestion
 
 resource "aws_s3_bucket" "ingest_bucket" {
   bucket = "energy-market-france"
@@ -26,8 +36,8 @@ resource "aws_ssm_parameter" "file_url" {
   }
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda-s3-access-role"
+resource "aws_iam_role" "ingestion_lambda_role" {
+  name = "lambda-ingestion-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -38,26 +48,35 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_iam_policy" "lambda_policy" {
-  name   = "lambda-s3-policy"
+resource "aws_iam_policy" "ingestion_lambda_policy" {
+  name   = "lambda-ingestion-policy"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       { Action = ["s3:PutObject", "s3:GetObject"], Effect = "Allow", Resource = ["${aws_s3_bucket.ingest_bucket.arn}/*"] },
-      { Action = ["ssm:GetParameter"], Effect = "Allow", Resource = "*" }
+      { Action = ["ssm:GetParameter"], Effect = "Allow", Resource = "*" },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect = "Allow",
+        Resource = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:*"
+      }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_role_attach" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
+resource "aws_iam_role_policy_attachment" "ingestion_lambda_attach" {
+  role       = aws_iam_role.ingestion_lambda_role.name
+  policy_arn = aws_iam_policy.ingestion_lambda_policy.arn
 }
 
 resource "aws_lambda_function" "ingestion" {
   filename         = "${path.module}/../lambdas/node/dist/ingestion.zip"
   function_name    = "energy-market-france-ingestion"
-  role             = aws_iam_role.lambda_role.arn
+  role             = aws_iam_role.ingestion_lambda_role.arn
   handler          = "index.handler"
   runtime          = "nodejs18.x"
   source_code_hash = filebase64sha256("${path.module}/../lambdas/node/dist/ingestion.zip")
@@ -69,4 +88,110 @@ resource "aws_lambda_function" "ingestion" {
       S3_BUCKET = aws_s3_bucket.ingest_bucket.id
     }
   }
+}
+
+# storage
+
+resource "aws_ssm_parameter" "db_dsn" {
+  name  = "/energy-market-france/db-dsn"
+  type  = "String"
+  value = "PLACEHOLDER"
+
+  lifecycle {
+    ignore_changes = [value, version]
+  }
+}
+
+resource "aws_lambda_layer_version" "pandas_layer" {
+  filename          = "${path.module}/../lambdas/python/dist/pandas-layer.zip"
+  layer_name        = "pandas-layer"
+  compatible_runtimes = ["python3.9"]
+}
+
+resource "aws_iam_role" "upsert_lambda_role" {
+  name = "lambda-upsert-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_upsert_policy" {
+  name   = "lambda-upsert-policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = ["s3:GetObject"],
+        Effect = "Allow",
+        Resource = "${aws_s3_bucket.ingest_bucket.arn}/*"
+      },
+      {
+        Action = ["ssm:GetParameter"],
+        Effect = "Allow",
+        Resource = "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/energy-market-france/db-dsn"
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect = "Allow",
+        Resource = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "upsert_lambda_attach" {
+  role       = aws_iam_role.upsert_lambda_role.name
+  policy_arn = aws_iam_policy.lambda_upsert_policy.arn
+}
+resource "aws_iam_role_policy_attachment" "lambda_insights_attach" {
+  role       = aws_iam_role.upsert_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy"
+}
+
+
+resource "aws_lambda_function" "upsert" {
+  filename         = "${path.module}/../lambdas/python/dist/upsert.zip"
+  function_name    = "energy-market-france-upsert"
+  role             = aws_iam_role.upsert_lambda_role.arn
+  handler          = "src/upsert/lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256("${path.module}/../lambdas/python/dist/upsert.zip")
+
+  memory_size      = 512  # Increase to load files into pandas dataframe
+  timeout          = 600 # Set timeout in seconds (maximum: 900 seconds)
+  layers           = [
+    aws_lambda_layer_version.pandas_layer.arn,
+    "arn:aws:lambda:eu-north-1:580247275435:layer:LambdaInsightsExtension:14"
+  ]
+}
+
+resource "aws_s3_bucket_notification" "ingest_bucket_notifications" {
+  bucket = aws_s3_bucket.ingest_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.upsert.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "data/"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3_to_invoke]
+}
+
+resource "aws_lambda_permission" "allow_s3_to_invoke" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.upsert.function_name
+  principal     = "s3.amazonaws.com"
+
+  source_arn = aws_s3_bucket.ingest_bucket.arn
 }
