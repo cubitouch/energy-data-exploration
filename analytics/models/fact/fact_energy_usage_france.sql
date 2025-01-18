5b31ebf48507e9ef5bbc1456e55b97ed54369696
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-01-18 10:32:20 +0100

    chore: add co2 impact analytics

--- analytics/models/fact/fact_energy_usage_france.sql
+++ analytics/models/fact/fact_energy_usage_france.sql
@@ -6,6 +6,7 @@ SELECT
     MIN(actual) AS usage_min,
     MAX(actual) AS usage_max,
     AVG(actual) AS usage_average,
+    SUM(co2_ratio * actual * 1000) AS co2_impact,
     SUM(renewable) AS usage_renewable,
     SUM(non_renewable) AS usage_non_renewable
 FROM {{ ref('stg_energy_market_france') }}
