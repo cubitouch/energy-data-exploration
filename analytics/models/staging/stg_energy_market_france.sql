a1c67f831a60b3c4d912992715e5299f71a84ad0
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-01-28 03:22:20 +0100

    chore: update sql typing

--- analytics/models/staging/stg_energy_market_france.sql
+++ analytics/models/staging/stg_energy_market_france.sql
@@ -4,77 +4,26 @@ WITH typed as (
     SELECT
         TO_TIMESTAMP(date || ' ' || heures, 'YYYY-MM-DD HH24:MI') AS t,
         consommation::NUMERIC AS actual,
-        CASE
-            WHEN pr_vision_j_1 IN ('', 'ND') THEN NULL
-            ELSE pr_vision_j_1::NUMERIC
-        END AS estimated_d1,
-        CASE
-            WHEN pr_vision_j IN ('', 'ND') THEN NULL
-            ELSE pr_vision_j::NUMERIC
-        END AS estimated_d,
+        pr_vision_j_1::NUMERIC AS estimated_d1,
+        pr_vision_j::NUMERIC AS estimated_d,
 
-        CASE
-            WHEN taux_de_co2 IN ('', 'ND') THEN NULL
-            ELSE taux_de_co2::NUMERIC
-        END AS co2_ratio,
+        taux_de_co2::NUMERIC AS co2_ratio,
 
-        CASE
-            WHEN fioul IN ('', 'ND') THEN 0
-            ELSE fioul::NUMERIC
-        END AS fuel_oil,
-        CASE
-            WHEN charbon IN ('', 'ND') THEN 0
-            ELSE charbon::NUMERIC
-        END AS coal,
-        CASE
-            WHEN gaz IN ('', 'ND') THEN 0
-            ELSE gaz::NUMERIC
-        END AS gas,
-        CASE
-            WHEN nucl_aire IN ('', 'ND') THEN 0
-            ELSE nucl_aire::NUMERIC
-        END AS nuclear,
-        CASE
-            WHEN eolien IN ('', 'ND') THEN 0
-            ELSE eolien::NUMERIC
-        END AS wind,
-        CASE
-            WHEN solaire IN ('', 'ND') THEN 0
-            ELSE solaire::NUMERIC
-        END AS solar,
-        CASE
-            WHEN hydraulique IN ('', 'ND') THEN 0
-            ELSE hydraulique::NUMERIC
-        END AS hydropower,
-        CASE
-            WHEN pompage IN ('', 'ND') THEN 0
-            ELSE pompage::NUMERIC
-        END AS pumped_storage,
-        CASE
-            WHEN bio_nergies IN ('', 'ND') THEN 0
-            ELSE bio_nergies::NUMERIC
-        END AS bioenergy,
+        fioul::NUMERIC AS fuel_oil,
+        charbon::NUMERIC AS coal,
+        gaz::NUMERIC AS gas,
+        nucl_aire::NUMERIC AS nuclear,
+        eolien::NUMERIC AS wind,
+        solaire::NUMERIC AS solar,
+        hydraulique::NUMERIC AS hydropower,
+        pompage::NUMERIC AS pumped_storage,
+        bio_nergies::NUMERIC AS bioenergy,
 
-        CASE
-            WHEN ech_comm_angleterre IN ('', 'ND') THEN 0
-            ELSE ech_comm_angleterre::NUMERIC
-        END AS import_england,
-        CASE
-            WHEN ech_comm_espagne IN ('', 'ND') THEN 0
-            ELSE ech_comm_espagne::NUMERIC
-        END AS import_spain,
-        CASE
-            WHEN ech_comm_italie IN ('', 'ND') THEN 0
-            ELSE ech_comm_italie::NUMERIC
-        END AS import_italy,
-        CASE
-            WHEN ech_comm_suisse IN ('', 'ND') THEN 0
-            ELSE ech_comm_suisse::NUMERIC
-        END AS import_swiss,        
-        CASE
-            WHEN ech_comm_allemagne_belgique IN ('', 'ND') THEN 0
-            ELSE ech_comm_allemagne_belgique::NUMERIC
-        END AS import_germany_belgium
+        ech_comm_angleterre::NUMERIC AS import_england,
+        ech_comm_espagne::NUMERIC AS import_spain,
+        ech_comm_italie::NUMERIC AS import_italy,
+        ech_comm_suisse::NUMERIC AS import_swiss,        
+        ech_comm_allemagne_belgique::NUMERIC AS import_germany_belgium
     FROM {{ source('raw_energy', 'raw_energy_market_france') }}
 )
 
