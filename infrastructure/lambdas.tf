d3582e62e01795671f5aa39868b9fa516c23f085
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-01-28 18:55:34 +0100

    chore: register new lambda to ingest energy performance reports

--- infrastructure/lambdas.tf
+++ infrastructure/lambdas.tf
@@ -175,3 +175,68 @@ resource "aws_lambda_permission" "allow_s3_to_invoke" {
 
   source_arn = aws_s3_bucket.ingest_bucket.arn
 }
+
+
+# ingest_energy_reports lambda
+resource "aws_iam_role" "ingest_energy_reports_lambda_role" {
+  name = "lambda-ingest-energy-reports-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_ingest_energy_reports_policy" {
+  name   = "lambda-ingest-energy-reports-policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = ["ssm:GetParameter"],
+        Effect = "Allow",
+        Resource = "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/energy-market-france/db-dsn"
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect = "Allow",
+        Resource = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ingest_energy_reports_lambda_attach" {
+  role       = aws_iam_role.ingest_energy_reports_lambda_role.name
+  policy_arn = aws_iam_policy.lambda_ingest_energy_reports_policy.arn
+}
+
+resource "aws_lambda_function" "ingest_energy_reports" {
+  function_name    = "energy-market-france-ingest-energy-reports"
+  role             = aws_iam_role.ingest_energy_reports_lambda_role.arn
+  package_type     = "Image"
+  image_uri        = "${aws_ecr_repository.lambda_upsert_repo.repository_url}:latest"
+
+  memory_size      = 512  # Increase to load files into pandas dataframe
+  timeout          = 600 # Set timeout in seconds (maximum: 900 seconds)
+  publish          = true # updates the function when new image is published
+  
+  # Override the default handler with the specific handler for this Lambda
+  image_config {
+    command = ["ingestion_energy_performance.lambda_function.lambda_handler"]
+  }
+  
+  # Ensure CloudWatch Lambda Insights layer for container support is added if needed
+  environment {
+    variables = {
+      LOG_LEVEL = "DEBUG" # Example variable
+    }
+  }
+}
