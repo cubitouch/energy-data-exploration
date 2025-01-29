7e5c86c9defb333dfae9eed498c9c7ad45def99f
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-01-29 12:52:08 +0100

    chore: fix data quality report

--- dashboards/src/data/data-quality.csv.ts
+++ dashboards/src/data/data-quality.csv.ts
@@ -3,7 +3,7 @@ import { safeFetchData } from "../utils/data-loader.js";
 const QUERY = `
   SELECT
     DATE_TRUNC('day', t) as timestamp_date,
-    SUM(CASE WHEN actual IS NOT NULL THEN 1 ELSE 0 END) as count
+    SUM(CASE WHEN actual > 0 THEN 1 ELSE 0 END) as count
   FROM analytics_staging.stg_energy_market_france
   GROUP BY 1
   ORDER BY 1
