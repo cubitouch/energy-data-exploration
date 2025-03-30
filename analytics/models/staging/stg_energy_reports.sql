3163d4b94c36401023a9b8a7d3a3a728fd940f0d
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-03-30 18:31:03 +0200

    feat: update data transformation

--- analytics/models/staging/stg_energy_reports.sql
+++ analytics/models/staging/stg_energy_reports.sql
@@ -1,12 +1,12 @@
 {{ config(materialized='table') }}
 
 SELECT
-    n_dpe AS id,
-    TO_TIMESTAMP(date_tablissement_dpe, 'YYYY-MM-DD') AS report_date,
+    numero_dpe AS id,
+    TO_TIMESTAMP(date_etablissement_dpe, 'YYYY-MM-DD') AS report_date,
     etiquette_ges AS ges_label,
     etiquette_dpe AS dpe_label,
-    ann_e_construction AS building_year,
-    code_postal_banx AS postal_code,
-    position_logement_dans_immeuble AS building_position,
-    replace(replace(p_riode_construction, 'avant', 'before'), 'après', 'after') AS construction_period
-FROM {{ source('raw_performance', 'energy_reports') }}
+    annee_construction AS building_year,
+    code_postal_ban AS postal_code,
+    numero_etage_appartement AS floor_number,
+    replace(replace(periode_construction, 'avant', 'before'), 'après', 'after') AS construction_period
+FROM {{ source('raw_performance', 'energy_reports_2025') }}
