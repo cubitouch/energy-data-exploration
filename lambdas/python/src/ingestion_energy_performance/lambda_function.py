df84cbc0a308c00e37ee92df8cd2a850d832728f
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-03-30 18:30:18 +0200

    feat: migrate EPC ingestion to new datasource

--- lambdas/python/src/ingestion_energy_performance/lambda_function.py
+++ lambdas/python/src/ingestion_energy_performance/lambda_function.py
@@ -1,4 +1,3 @@
-
 import boto3
 import pandas as pd
 import urllib.parse
@@ -15,47 +14,56 @@ import dlt
 # Initialize S3 and SSM clients
 ssm = boto3.client("ssm")
 
+
 # REST API source for querying one district
 def energy_reports_source():
-    return rest_api_source({
-        "client": {
-            "base_url": "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v2-logements-existants/",
-            "paginator": {
-                "type": "json_link",
-                "next_url_path": "next",
-            },
-        },
-        "resource_defaults": {
-            "primary_key": "N°DPE",
-            "write_disposition": "merge",
-            "table_name": "energy_reports",
-            "endpoint": {
-                "params": {
-                    "size": 10_000,
-                    "sort": "Date_réception_DPE",
-                    "select": "N°DPE,Date_établissement_DPE,Etiquette_GES,Etiquette_DPE,Année_construction,Type_installation_ECS_(général),Période_construction,Surface_habitable_logement,Classe_inertie_bâtiment,Typologie_logement,Position_logement_dans_immeuble,Nom__commune_(BAN),N°_région_(BAN),Code_postal_(BAN)",
+    return rest_api_source(
+        {
+            "client": {
+                "base_url": "https://data.ademe.fr/data-fair/api/v1/datasets/dpe03existant/",
+                "paginator": {
+                    "type": "json_link",
+                    "next_url_path": "next",
                 },
             },
-        },
-        "resources": [
-            {
-                "name": f"district_{postal_code}",
+            "resource_defaults": {
+                "primary_key": "numero_dpe",
+                "write_disposition": "merge",
+                "table_name": "energy_reports_2025",
                 "endpoint": {
-                    "path": "lines", 
                     "params": {
-                        "q_fields": "Code_postal_(BAN)",
-                        "q": postal_code,  # Postal code for each district
+                        "size": 10_000,
+                        "sort": "date_reception_dpe",
+                        "select": "numero_dpe,date_etablissement_dpe,etiquette_ges,etiquette_dpe,annee_construction,type_installation_ecs,periode_construction,surface_habitable_logement,classe_inertie_batiment,numero_etage_appartement,nom_commune_ban,code_region_ban,code_postal_ban",
                     },
                 },
-            }
-            for postal_code in [f"750{str(district).zfill(2)}" for district in range(1, 21)] + ["75116"]
-        ],
-    }, parallelized=True)
-    
+            },
+            "resources": [
+                {
+                    "name": f"district_{postal_code}",
+                    "endpoint": {
+                        "path": "lines",
+                        "params": {
+                            "q_fields": "code_postal_ban",
+                            "q": postal_code,  # Postal code for each district
+                        },
+                    },
+                }
+                for postal_code in [
+                    f"750{str(district).zfill(2)}" for district in range(1, 21)
+                ]
+                + ["75116"]
+            ],
+        },
+        parallelized=True,
+    )
+
 
 def lambda_handler(event, context):
     # Fetch the DSN from SSM Parameter Store
-    response = ssm.get_parameter(Name="/energy-market-france/db-dsn", WithDecryption=True)
+    response = ssm.get_parameter(
+        Name="/energy-market-france/db-dsn", WithDecryption=True
+    )
     database_dsn = response["Parameter"]["Value"]
     pipeline_destination = dlt.destinations.postgres(database_dsn)
 
@@ -92,5 +100,5 @@ def lambda_handler(event, context):
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     lambda_handler(None, None)
